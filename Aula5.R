66aa95d369d555e5c28793aa888f921a3c773a75
Victor Taouil <victorm.taouil@gmail.com>
2024-10-09 19:01:19 -0300

    commitando no intervalo

--- Aula5.R
+++ Aula5.R
@@ -58,5 +58,51 @@ teste == z
 seq(1:10)|>sqrt()|>log()
 teste
 
+# Mudando o nome de uma coluna 
 
+df_mh|>rename(gender = Gender)
+
+
+# mudando todas as colunas para tolower()
+
+
+
+colunas<- colnames(df_mh)
+df_mh|>rename(timestamp = Timestamp)
+
+### 
+
+
+
+require(magrittr)  
+set.seed(123)
+
+rnorm(10)|> multiply_by(5)|>add(5)
+
+
+#Pipe de atribuição 
+
+require(dplyr)
+## Atribuição explicita
+meu_data_frame <- data.frame(
+  nome = c("Alice", "Bob", "Carol", "Ana", "João", "Carlos", "Patrícia", "Leonardo"),
+  idade = c(25, 30, 28, 20, 27, 50, 60, 45),
+  salario = c(5000, 6000, 5500, 8000, 2000, 3500, 10000, 3800 ), 
+  meio_de_transporte = c('onibus', 'bicicleta', 'onibus', 'carro', 'carro', 'onibus', 'onibus', 'bicicleta'))
+
+meu_data_frame = meu_data_frame %>%
+  mutate(idade_25 = idade > 25)
+
+# ou podemos atribuir direto, sem precisar chamar de volta o 
+
+meu_data_frame %<>% mutate(idade_60 = idade>60)
+glimpse(meu_data_frame)
+meu_data_frame
+
+# Agora vamos mexer com o banco de dados df, que é o car crash 
+
+
+df2 <- readr::read_csv('/home/est/vmt24/CE302_2024/Data/Brazil Total highway crashes 2010 - 2023 (1).csv.gz')
+
+head(df2)
 
